ed318bd2bad36f1527a7abec759cd99d68a51c48
i-tereshchenko <i.tereschenko@weplay.tv>
2023-12-27 02:05:09 +0200

    - submenu

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,9 +1,53 @@
+import {useState} from "react";
+import {Link, useLocation} from "react-router-dom";
+
+import classNames from "classnames";
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {Link} from "react-router-dom";
 
 import style from './index.module.scss';
 
 const Nav = () => {
+	const { pathname } = useLocation();
+	const [menu, setMenu] = useState([
+		{
+			text: 'Accounts',
+			icon: 'fa-solid fa-wallet',
+			active: false,
+			submenu: [
+				{
+					text: 'Accounts',
+					link: '/accounts'
+				}
+			]
+		},
+		{
+			text: 'Ticket Management',
+			icon: 'fa-solid fa-money-bill',
+			active: false,
+			submenu: [
+				{
+					text: 'Tickets',
+					link: '/tickets'
+				}
+			]
+		},
+		{
+			text: 'Financial',
+			icon: 'fa-solid fa-credit-card',
+			active: false,
+			submenu: []
+		}
+	])
+	
+	const setActive = (index, value) => {
+		setMenu((prevMenu) => {
+			const newMenu = [...prevMenu];
+			newMenu[index].active = value;
+			return newMenu;
+		});
+	};
+	
     return (
         <nav className={style.block}>
 			<div className={style.wrapper}>
@@ -15,65 +59,62 @@ const Nav = () => {
 				</div>
 				<hr/>
 				<ul>
-					<li className={style.item}>
-						<span className={style.link}>
-							<FontAwesomeIcon
-								icon="fa-solid fa-wallet"
-								className={style.icon}
-							/>
-							<span>Accounts</span>
-							<FontAwesomeIcon
-								icon="fa-solid fa-angle-down"
-								className={style.arrow}
-							/>
-						</span>
-						<div className={style.submenu}>
-							<Link
-								to={`/accounts`}
-								rel="noreferrer"
-								className={style.link}
+					{
+						menu.map((el, idx) =>
+							<li
+								key={idx}
+								className={
+									classNames(
+										style.item,
+										el.active && style.active
+									)
+								}
 							>
-								<i className={style.icon} />
-								<span>Accounts</span>
-							</Link>
-						</div>
-					</li>
-					<li className={style.item}>
-						<span className={style.link}>
-							<FontAwesomeIcon
-								icon="fa-solid fa-money-bill"
-								className={style.icon}
-							/>
-							<span>Ticket Management</span>
-							<FontAwesomeIcon
-								icon="fa-solid fa-angle-down"
-								className={style.arrow}
-							/>
-						</span>
-						<div className={style.submenu}>
-							<Link
-								to={`/tickets`}
-								rel="noreferrer"
-								className={style.link}
-							>
-								<i className={style.icon} />
-								<span>Tickets</span>
-							</Link>
-						</div>
-					</li>
-					<li className={style.item}>
-						<span className={style.link}>
-							<FontAwesomeIcon
-								icon="fa-solid fa-credit-card"
-								className={style.icon}
-							/>
-							<span>Financial</span>
-							<FontAwesomeIcon
-								icon="fa-solid fa-angle-down"
-								className={style.arrow}
-							/>
-						</span>
-					</li>
+								<span
+									onClick={() => {
+										setActive(idx, !el.active)
+									}}
+									className={style.link}
+								>
+									<FontAwesomeIcon
+										icon={el.icon}
+										className={style.icon}
+									/>
+									<span>{el.text}</span>
+									{
+										el.submenu.length > 0 &&
+										<FontAwesomeIcon
+											icon="fa-solid fa-angle-down"
+											className={style.arrow}
+										/>
+									}
+								</span>
+								{
+									el.submenu.length > 0 &&
+									<div className={style.submenu}>
+										{
+											el.submenu.map((el_s, idx_s) =>
+												<Link
+													key={idx_s}
+													to={el_s.link}
+													rel="noreferrer"
+													className={
+														classNames(
+															style.link,
+															pathname === el_s.link && style.active
+														)
+													}
+												>
+													<i className={style.icon} />
+													<span>{el_s.text}</span>
+												</Link>
+											)
+										}
+									</div>
+								}
+							</li>
+						)
+					}
 				</ul>
 			</div>
         </nav>
