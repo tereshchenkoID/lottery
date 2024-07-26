351540264d6620769fcb837fb74ea793eeb09723
tereshchenkoID <teren23041991@ukr.net>
2024-07-26 17:07:24 +0300

    News page

---
+++ src/pages/News/Article/index.js
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom'
+import { useLoading } from 'hooks/useLoading'
+
+import YouTube from 'react-youtube'
+
+import { getDate } from 'helpers/getDate'
+
+import Skeleton from 'components/Skeleton'
+
+import style from './index.module.scss'
+
+const Article = ({ data }) => {
+  const [loading] = useLoading(true)
+
+  const opts = {
+    playerVars: {
+      autoplay: 1,
+    },
+  }
+
+  return (
+    <article className={style.block}>
+      {
+        loading &&
+        <Skeleton
+          styles={{
+            position: 'absolute',
+            zIndex: '1',
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      }
+      <Link
+        to={`/news/${data.id}`}
+        rel="noreferrer"
+        className={style.media}
+      >
+      {
+        data.photo &&
+        <img 
+          src={data.photo}
+          loading={'lazy'}
+          alt={'Preview'} 
+        />
+      }
+      {
+        data.video &&
+        <YouTube videoId={data.video} opts={opts} />
+      }
+      </Link>
+      <p className={style.date}>{getDate(data.date, 3)}</p>
+      <h6 className={style.title}>
+        <Link
+          to={`/news/${data.id}`}
+          rel="noreferrer"
+          className={style.link}
+        >
+          {data.title}
+        </Link>
+      </h6>
+      <p className={style.description}>{data.snipet}</p>
+    </article>
+  )
+}
+
+export default Article
--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,15 +1,75 @@
+import { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 
 import { NAVIGATION } from 'constant/config'
 
+import { postData } from 'helpers/api'
+
 import Container from 'components/Container'
 import Title from 'components/Title'
+import Loader from 'components/Loader'
 import Breadcrumbs from 'modules/Breadcrumbs'
+import Pagination from 'modules/Pagination'
+import Empty from 'modules/Empty'
+import Article from './Article'
 
 import style from './index.module.scss'
 
 const News = () => {
   const { t } = useTranslation()
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  const [pagination, setPagination] = useState({
+    page: 0,
+    pages: 0,
+    quantity: 0,
+    results: 0,
+  })
+
+  const handleLoad = (page) => {
+    setLoading(true)
+
+    const formData = new FormData()
+    formData.append('page', page)
+
+    postData('news/', formData).then(json => {
+      setData(json.data)
+
+      setPagination({
+        page: json.page,
+        pages: json.pages,
+        quantity: json.quantity,
+        results: json.results,
+      })
+
+      setLoading(false)
+    })
+  }
+
+  const handlePagination = (fieldName, fieldValue) => {
+    setPagination(prevPagination => ({
+      ...prevPagination,
+      [fieldName]: fieldValue,
+    }))
+    handleLoad(fieldValue)
+  }
+
+  const handlePrev = () => {
+    const prev = pagination.page > 0 ? pagination.page - 1 : 0
+    handlePagination('page', prev)
+  }
+
+  const handleNext = () => {
+    const next =
+      pagination.page < pagination.pages
+        ? pagination.page + 1
+        : pagination.pages
+    handlePagination('page', next)
+  }
+
+  useEffect(() => {
+    handleLoad(0)
+  }, [])
 
   return (
     <Container>
@@ -19,7 +79,42 @@ const News = () => {
         ]}
       />
       <Title text={t(NAVIGATION.news.text)} />
-      <div className={style.grid}></div>
+      {
+        loading 
+        ?
+          <Loader type={'inline'} />
+        :
+          <>
+            {
+              data?.length > 0 
+              ?
+                <div className={style.list}>
+                  <div className={style.grid}>
+                    {
+                      data.map((el, idx) => 
+                        <Article 
+                          key={idx}
+                          data={el} 
+                        />
+                      )
+                    }
+                  </div>
+                  {
+                    pagination.pages > 1 &&
+                    <div className={style.pagination}>
+                      <Pagination
+                        pagination={pagination}
+                        handlePrev={() => handlePrev()}
+                        handleNext={() => handleNext()}
+                      />
+                    </div>
+                  }
+                </div>
+              :
+                <Empty />
+            }
+          </>
+        }
     </Container>
   )
 }
